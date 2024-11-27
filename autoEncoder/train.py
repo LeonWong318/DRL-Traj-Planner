afe5ae7c6af5e975b7164c10ce6a798c841d5bc9
LeonWong318 <wangyinsong01@gmail.com>
2024-11-27 15:14:31 +0100

    add loss plot function during training

--- autoEncoder/train.py
+++ autoEncoder/train.py
@@ -1,10 +1,29 @@
 import torch
 from pytorch_lightning import Trainer
-from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
+from pytorch_lightning.callbacks import ModelCheckpoint, Callback
 import argparse
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+
 
 from autoEncoder import Autoencoder
 from dataLoad import create_dataloaders
+
+# To store the loss
+train_losses = []
+val_losses = []
+class LossLoggerCallback(Callback):
+    def on_train_epoch_end(self, trainer, pl_module):
+        global train_losses, val_losses
+
+        # Access logged metrics
+        train_loss = trainer.logged_metrics.get("train_loss")
+        val_loss = trainer.logged_metrics.get("val_loss")
+        
+        train_losses.append(float(train_loss.item()))  # Convert tensor to float
+        val_losses.append(float(val_loss.item()))  # Convert tensor to float
+            
 def parse_args():
     parser = argparse.ArgumentParser(description='Train a simple model')
     parser.add_argument('--ld', type=int, default=128, help='Dimensionality of the latent space')
@@ -46,20 +65,39 @@ def main():
     #     patience=10,
     #     mode="min"
     # )
-
+    
+    loss_logger = LossLoggerCallback()
     # Trainer
     trainer = Trainer(
         max_epochs=num_epochs,
         #callbacks=[checkpoint_callback, early_stopping_callback],
-        callbacks = checkpoint_callback,
+        callbacks = [checkpoint_callback, loss_logger],
         accelerator="auto",  # Use GPU if available
+        logger=False,  # Disable Lightning loggers
     )
-
+    
+    
     # Train the model
     trainer.fit(model, train_loader, val_loader)
-
+    #print("Train Loss:", trainer.logged_metrics.get("train_loss").item())
+    #print("Validation Loss:", trainer.logged_metrics.get("val_loss").item())
     # Save the final model
-    torch.save(model.state_dict(), f"./model/autoencoder_alldata_{latent_dim}e{num_epochs}.pth")
+    torch.save(model.state_dict(), f"./model/autoencoder_allnewdata_{latent_dim}e{num_epochs}.pth")
+    
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    # Plot and save the loss curve
+    plt.figure(figsize=(10, 6))
+    plt.plot(train_losses, label='Train Loss')
+    plt.plot(val_losses, label='Validation Loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.title(f'Training and Validation Loss for {latent_dim} Latent over {num_epochs} Epochs')
+    plt.legend()
+    # Ensure x-axis shows only integers
+    epochs = range(0, len(train_losses))
+    plt.xticks(epochs)  # Set x-ticks to integers (0-based index)
+    plt.savefig(f'autoencoder_allnewdata_{latent_dim}e{num_epochs}_loss_curve_{timestamp}.png')
+    plt.show()
 
 if __name__ == "__main__":
     main()
